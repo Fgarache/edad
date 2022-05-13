625f277adfa81a069a9316d2ae4869f7772d81fe
Francisco Garache <franciscogarach3@gmail.com>
2022-05-13 23:11:41 +0000

    operadores logicos

--- main.swift
+++ main.swift
@@ -18,3 +18,69 @@ print (colorNameToUse)
 let user1 = edadUser ?? edad
 
 print (user1)
+
+
+for idx1 in 1...10{
+  print(idx1)
+}
+
+for idx in 1..<5{
+  print(" ")
+  print(idx)
+}
+
+let names = ["ricardo","juan","roger","frank","kevin"]
+
+//imprime todos los nombres
+for i in 0..<names.count{
+  print("la persona \(i+1) se llama \(names[i])")
+}
+print(" ")
+
+//imprime asta llegar al final
+for name in names[1...]{
+  print(name)
+}
+print(" ")
+
+//solo imprime el valor asignado
+ for name in names[..<2]{
+   print(name)
+ }
+
+//rangos
+let range = ...5
+//este rangp contiene el 7?
+range.contains(7) //false 
+range.contains(4) //true
+range.contains(-4) //true
+
+let permitirEntrada = false
+//si no permitimos entrada inprimer acceso denegado
+ if !permitirEntrada{
+   print("acceso denegado")
+      print("acceso denegado")
+   print(" ")
+print(" ")
+ }
+
+let introCodec = true
+let passRetiScam = true
+
+//si codigo es true y pass es true puedes pasar
+if introCodec && passRetiScam {
+  print("puedes entrar")
+}else{
+  print("acceso negado")
+}
+ print(" ")
+print(" ")
+ print(" ")
+print(" ")
+
+let money = false
+let invitacion = true
+//si tengo money o tengo invitacion puedo entrar
+if money || invitacion {
+  print("Bienvenido a la fiesta")
+}else{print("Acceso negado")}
